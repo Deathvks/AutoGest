e858e177488edc0476a22c4f897171eda1338eb1
Deathvks <dylanjesussuarez@gmail.com>
2025-09-10 11:27:38 +0100

    añadido muchas cosas

--- backend/ecosystem.config.js
+++ backend/ecosystem.config.js
@@ -12,7 +12,7 @@ module.exports = {
        // --- Configuración de la Base de Datos de PRODUCCIÓN ---
        DB_HOST: "localhost",
        DB_USER: "autogest_user",
-       DB_PASSWORD: "aqxt9m!$CEufWrcsSe2!YhLLUvG8ReBEBDqWM@acWsqbWJTR2d5C#JKaCgJ^Ag&f",
+       DB_PASSWORD: "cr18r4t10N_hu85_4Mpull43_v1nt_200Ph0814_w3ld_ch4Ml3t_d1R3r_unl1nE_80rd4r",
        DB_NAME: "autogest_db",
        // ... aquí puedes añadir el resto de variables de producción
     }
