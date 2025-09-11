d204900ad99bed4f06542285cb9fc3e83ac03d94
Deathvks <dylanjesussuarez@gmail.com>
2025-09-11 17:07:40 +0100

    añadido soporte fotos

--- backend/ecosystem.config.js
+++ backend/ecosystem.config.js
@@ -3,18 +3,16 @@ module.exports = {
   apps : [{
     name   : "backend",
     script : "./index.js",
-    // --- INICIO DE LA MODIFICACIÓN ---
-    // Ignorar el fichero .env en producción para evitar conflictos
     ignore_watch: [".env"],
-    // --- FIN DE LA MODIFICACIÓN ---
     env_production: {
        NODE_ENV: "production",
-       // --- Configuración de la Base de Datos de PRODUCCIÓN ---
        DB_HOST: "localhost",
        DB_USER: "autogest_user",
        DB_PASSWORD: "cr18r4t10N_hu85_4Mpull43_v1nt_200Ph0814_w3ld_ch4Ml3t_d1R3r_unl1nE_80rd4r",
        DB_NAME: "autogest_db",
-       // ... aquí puedes añadir el resto de variables de producción
+       // --- INICIO DE LA MODIFICACIÓN ---
+       DB_DIALECT: "mysql", // <-- Esta es la línea que faltaba
+       // --- FIN DE LA MODIFICACIÓN ---
     }
   }]
 }
