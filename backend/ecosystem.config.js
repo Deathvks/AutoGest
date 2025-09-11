e731a30e76363f8fcae71c17d355ec6cf87a1df1
Deathvks <dylanjesussuarez@gmail.com>
2025-09-11 17:38:17 +0100

    ahora si

--- backend/ecosystem.config.js
+++ backend/ecosystem.config.js
@@ -8,7 +8,7 @@ module.exports = {
        NODE_ENV: "production",
        DB_HOST: "localhost",
        DB_USER: "autogest_user",
-       DB_PASSWORD: "cr18r4t10N_hu85_4Mpull43_v1nt_200Ph0814_w3ld_ch4Ml3t_d1R3r_unl1nE_80rd4r",
+       DB_PASSWORD: "cr18r4t10N_hu85_4Mpull43_v1nt_200Ph0814_wld_ch4Ml3t_d1R3r_unl1nE_80rd4r",
        DB_NAME: "autogest_db",
        // --- INICIO DE LA MODIFICACIÓN ---
        DB_DIALECT: "mysql", // <-- Esta es la línea que faltaba
